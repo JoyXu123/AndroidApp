6b9d199326831f5682bc5f919f984f6d6f0cb5fd
JoyXu123 <xu.jingyi1@husky.neu.edu>
2021-05-16 16:57:36 -0400

    Add About Button

--- app/src/main/java/edu/neu/madcourse/numad21su_jingyixu/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21su_jingyixu/MainActivity.java
@@ -3,6 +3,9 @@ package edu.neu.madcourse.numad21su_jingyixu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Create a "About" Button, two steps
+        //get the button
+        Button abt = findViewById(R.id.About);
+        //... set what happens when user click
+        abt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Name: Jingyi Xu \n Email: xu.jingyi1@northeastern.edu", Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
     }
 }
